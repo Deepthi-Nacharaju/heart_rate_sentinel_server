94935aa43cf3e70890b919af9797a8468f2d5170
Deepthi Nacharaju <dnacharaju@gmail.com>
2018-11-11 15:51:03 -0500

    add send_email function

--- HRSS.py
+++ HRSS.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route("/api/new_patient", methods=["POST"])
 def new_patient():
-    r = requests.get_json()
+    r = request.get_json()
     return jsonify(r)
 
 
 @app.route("/api/heart_rate", methods=["POST"])
 def heart_rate():
+    r = request.get_json()
+    return jsonify(r)
 
-    return jsonify(dictionary)
 
+if __name__ == "__main__":
+    app.run(host="127.0.0.1")
