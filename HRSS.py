4b607b8b308deea681719875401f796b495ad957
Deepthi Nacharaju <dnacharaju@gmail.com>
2018-11-11 15:19:27 -0500

    add heart rate post request

--- HRSS.py
+++ HRSS.py
@@ -7,14 +7,11 @@ app = Flask(__name__)
 @app.route("/api/new_patient", methods=["POST"])
 def new_patient():
     r = requests.get_json()
-    return jsonify(dictionary)
+    return jsonify(r)
 
 
 @app.route("/api/heart_rate", methods=["POST"])
 def heart_rate():
-    dictionary = {
-        "patient_id": "1", # usually this would be the patient MRN
-        "heart_rate": 100
-    }
+
     return jsonify(dictionary)
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+#  import os
 
 def post_new_patient(parameters, server=None):
     """
@@ -27,8 +27,23 @@ def post_new_patient(parameters, server=None):
     return
 
 
-def main():
+def post_heart_rate(parameters, server=None):
+    if not server:
+        server = "http://127.0.0.1:5000/api/heart_rate"
+    else:
+        server = server + '/api/heart_rate'
+    post_dictionary = {"patient_id": parameters[0],  # usually this would be the patient MRN
+                       "heart_rate": parameters[1],
+                       }
+    try:
+        r = requests.post(server, json=post_dictionary)
+    except:
+        print("Check Inputs")
+    return print(r.json())
 
+
+def main():
+    #  os.system("FLASK_APP=flask_service.py flask run")
     return
 
 
